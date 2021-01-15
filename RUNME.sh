ce46ed2a627aa3d5b23400c7b97d9d8fa8a63544
miguel <miguelcok27@gmail.com>
2021-01-15 13:29:55 -0500

    added tags to package.json. Added GIT_REPOSITORY tag to RUNME.sh

--- RUNME.sh
+++ RUNME.sh
@@ -12,10 +12,11 @@ read -e -p "Company name: " -i "FunnyCode" COMPANY_FRIENDLY_NAME
 read -e -p "Company name in lower case: " -i "funnycode" COMPANY
 read -e -p "Unacceptable behavior may be reported at: " -i "" REPORT_EMAIL
 read -e -p "Company website: " -i "" COMPANY_WEBSITE
-read -e -p "Repository name: " -i "unity-simple-notifications" REPOSITORY_NAME
-read -e -p "Friendly name: " -i "Notifications" FRIENDLY_NAME
-read -e -p "Description: " -i "Package for Unity game engine." DESCRIPTION
-read -e -p "Unity version: " -i "2019.4" UNITY_VERSION
+read -e -p "Repository name: " -i "" REPOSITORY_NAME
+read -e -p "Friendly name: " -i "" FRIENDLY_NAME
+read -e -p "Description: " -i "" DESCRIPTION
+read -e -p "Unity version: " -i "2019.3" UNITY_VERSION
+read -e -p "Git repo: " -i "2019.3" GIT_REPOSITORY
 
 # Escape special characters for input to be used in sed
 COMPANY_FRIENDLY_NAME=$(echo "$COMPANY_FRIENDLY_NAME" | sed -e 's/[]\/$*.^[]/\\&/g');
@@ -26,6 +27,7 @@ REPOSITORY_NAME=$(echo "$REPOSITORY_NAME" | sed -e 's/[]\/$*.^[]/\\&/g');
 FRIENDLY_NAME=$(echo "$FRIENDLY_NAME" | sed -e 's/[]\/$*.^[]/\\&/g');
 DESCRIPTION=$(echo "$DESCRIPTION" | sed -e 's/[]\/$*.^[]/\\&/g');
 UNITY_VERSION=$(echo "$UNITY_VERSION" | sed -e 's/[]\/$*.^[]/\\&/g');
+GIT_REPOSITORY=$(echo "$GIT_REPOSITORY" | sed -e 's/[]\/$*.^[]/\\&/g');
 
 echo 'Replacing template strings...'
 
@@ -33,7 +35,7 @@ echo 'Replacing template strings...'
 YEAR="$(date +'%Y')"
 
 # Form sed command and store it into a file. Ran into problems with white spaces when trying to pass this as parameter. 
-echo "s/{{REPOSITORY_NAME}}/"${REPOSITORY_NAME}"/g;s/{{FRIENDLY_NAME}}/"${FRIENDLY_NAME}"/g;s/{{DESCRIPTION}}/"${DESCRIPTION}"/g;s/{{UNITY_VERSION}}/"${UNITY_VERSION}"/g;s/{{COMPANY}}/"${COMPANY}"/g;s/{{COMPANY_FRIENDLY_NAME}}/"${COMPANY_FRIENDLY_NAME}"/g;s/{{YEAR}}/"${YEAR}"/g;s/{{COMPANY_WEBSITE}}/"${COMPANY_WEBSITE}"/g;s/{{REPORT_EMAIL}}/"${REPORT_EMAIL}"/g" > temp.txt
+echo "s/{{REPOSITORY_NAME}}/"${REPOSITORY_NAME}"/g;s/{{FRIENDLY_NAME}}/"${FRIENDLY_NAME}"/g;s/{{DESCRIPTION}}/"${DESCRIPTION}"/g;s/{{UNITY_VERSION}}/"${UNITY_VERSION}"/g;s/{{COMPANY}}/"${COMPANY}"/g;s/{{COMPANY_FRIENDLY_NAME}}/"${COMPANY_FRIENDLY_NAME}"/g;s/{{YEAR}}/"${YEAR}"/g;s/{{COMPANY_WEBSITE}}/"${COMPANY_WEBSITE}"/g;s/{{REPORT_EMAIL}}/"${REPORT_EMAIL}"/g;s/{{GIT_REPOSITORY}}/"${GIT_REPOSITORY}"/g" > temp.txt
 
 ( shopt -s globstar dotglob;
     for file in **; do
@@ -52,22 +54,6 @@ echo "s/{{REPOSITORY_NAME}}/"${REPOSITORY_NAME}"/g;s/{{FRIENDLY_NAME}}/"${FRIEND
 
 rm temp.txt
 echo 'done.'
-echo 'Removing template repository specific files...'
-
-# Remove template repository specific files
-rm README.md
-rm CONTRIBUTING.md
-rm LICENSE
-rm package.json
-rm .github/CODEOWNERS
-rmdir .github
-mv templates/README.md README.md
-mv templates/CONTRIBUTING.md CONTRIBUTING.md
-mv templates/LICENSE LICENSE
-mv templates/package.json package.json
-mv templates/.github .github
-
-rmdir templates
 
 echo 'done.'
 rm RUNME.sh
